a947df8a6ead3b58e1f53a3e3b2b5a8a6d56bc55
vkodithala <varoon.kodithala@gmail.com>
2025-05-04 00:15:07 -0400

    better copy, maybe better mobile view?

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Varoon's mind garden 🪴",
+    pageTitle: "🪴 Varoon's mind garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -29,7 +29,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#F2F1F1",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
